6d171f580c69631748b7f6fb8c94c58da0effac5
Luis de Dios Martín <luisddm@gmail.com>
2014-12-17 23:14:58 +0100

    Now the board fields show the numbers or mines on click

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*! luisddm/minesweeper v0.1 | (c) 2014 https://github.com/luisddm/minesweeper | GNU GPL License */
 
 (function($) {
 
@@ -5,72 +6,97 @@
 
   $(document).ready(function(){
 
-    var colors = [null, "blue", "green", "red", "darkblue", "brown", "cyan", "black", "gray"];
+    var fgColors = ["white", "blue", "green", "red", "darkblue", "brown", "cyan", "black", "gray"];
+    var bgColors = ["black"];
 
     var nMines = 10;
     var boardSize = 8;
     var n = 0, i = 0, j = 0;
 
     var $board = $(".board");
+    var $time = $(".time");
 
-    populateMines();
+    setInterval(function() {
+      $time.text(+$time.text() + 1);
+    }, 1000);
 
-    function populateMines() {
-      while(n < nMines){
-        var x = Math.floor((Math.random() * boardSize));
-        var y = Math.floor((Math.random() * boardSize));
+    // Pupulate the board with mines in random positions
+    while(n < nMines) {
+      var x = randomNumber(boardSize);
+      var y = randomNumber(boardSize);
 
-        var $cell = $board.find("tr").eq(y).find("td").eq(x);
-        var content = $cell.text();
-        if(content == 'XX') {
-          $cell.text('O').css("background-color", "lightgray");
-          n++;
-        }
+      var $cell = $board.find("tr").eq(y).find("td").eq(x);
+      var content = $cell.data("mines");
+      if(content !== 0) {
+        $cell.data("mines", 0);
+        n++;
       }
-      populateNumbers();
     }
 
-    function populateNumbers() {
-      var $central;
-
-      for(i = 0; i < boardSize; i++) {
-        for(j = 0; j < boardSize; j++) {
-          var total = 0;
-          $central = $board.find("tr").eq(i).find("td").eq(j);
-          if($central.text() == 'XX') {
-            if(i-1 >= 0 && j-1 >= 0) {
-              if($board.find("tr").eq(i-1).find("td").eq(j-1).text() == 'O') total++;
-            }
-            if(i-1 >= 0) {
-              if($board.find("tr").eq(i-1).find("td").eq(j).text() == 'O') total++;
-            }
-            if(i-1 >= 0 && j+1 <= boardSize) {
-              if($board.find("tr").eq(i-1).find("td").eq(j+1).text() == 'O') total++;
-            }
-            if(j-1 >= 0) {
-              if($board.find("tr").eq(i).find("td").eq(j-1).text() == 'O') total++;
-            }
-            if(j+1 <= boardSize) {
-              if($board.find("tr").eq(i).find("td").eq(j+1).text() == 'O') total++;
-            }
-            if(i+1 <= boardSize && j-1 >= 0) {
-              if($board.find("tr").eq(i+1).find("td").eq(j-1).text() == 'O') total++;
-            }
-            if(i+1 <= boardSize) {
-              if($board.find("tr").eq(i+1).find("td").eq(j).text() == 'O') total++;
-            }
-            if(i+1 <= boardSize && j+1 <= boardSize) {
-              if($board.find("tr").eq(i+1).find("td").eq(j+1).text() == 'O') total++;
-            }
-            if(total > 0) {
-              $central.text(total).css("color", colors[total]);
-            } else {
-              $central.text("");
-            }
+    // Look all the fields one by one searching for how many mines are around
+    for(i = 0; i < boardSize; i++) {
+      for(j = 0; j < boardSize; j++) {
+        var total = 0;
+        var $central = $board.find("tr").eq(i).find("td").eq(j);
+        if($central.data("mines") !== 0) {
+          if(i-1 >= 0 && j-1 >= 0) {
+            if($board.find("tr").eq(i-1).find("td").eq(j-1).data("mines") === 0) total++;
+          }
+          if(i-1 >= 0) {
+            if($board.find("tr").eq(i-1).find("td").eq(j).data("mines") === 0) total++;
+          }
+          if(i-1 >= 0 && j+1 <= boardSize) {
+            if($board.find("tr").eq(i-1).find("td").eq(j+1).data("mines") === 0) total++;
+          }
+          if(j-1 >= 0) {
+            if($board.find("tr").eq(i).find("td").eq(j-1).data("mines") === 0) total++;
+          }
+          if(j+1 <= boardSize) {
+            if($board.find("tr").eq(i).find("td").eq(j+1).data("mines") === 0) total++;
+          }
+          if(i+1 <= boardSize && j-1 >= 0) {
+            if($board.find("tr").eq(i+1).find("td").eq(j-1).data("mines") === 0) total++;
+          }
+          if(i+1 <= boardSize) {
+            if($board.find("tr").eq(i+1).find("td").eq(j).data("mines") === 0) total++;
+          }
+          if(i+1 <= boardSize && j+1 <= boardSize) {
+            if($board.find("tr").eq(i+1).find("td").eq(j+1).data("mines") === 0) total++;
+          }
+          if(total > 0){
+            $central.data("mines", total);
           }
         }
       }
     }
+
+    // Click on any field to reveal the number or the mine
+    $board.find("td").on("click", function() {
+      var posX = $(this).index();
+      var posY = $(this).parent().index();
+      //alert("Pos "+posX+","+posY);
+
+      $(this).text($(this).data("mines")).css({
+        "color": fgColors[$(this).data("mines")],
+        "background-color": bgColors[$(this).data("mines")]
+      });
+    });
+
+    // Reveal al the board
+    $("h1").on("click", function() {
+      $board.find("td").each(function(index) {
+        $(this).text($(this).data("mines")).css({
+          "color": fgColors[$(this).data("mines")],
+          "background-color": bgColors[$(this).data("mines")]
+        });
+      });
+    });
+
   });
+
+  // Random number generation between 0 and boardSize-1
+  function randomNumber(boardSize) {
+    return Math.floor((Math.random() * boardSize));
+  }
 
 })(jQuery);
