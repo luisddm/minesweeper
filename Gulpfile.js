e2dba304b1dfba26adfef725bb1526b6e28ffd34
Luis de Dios Martín <luisddm@gmail.com>
2016-04-23 17:22:23 +0200

    Code styling upgrades

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,101 +1,96 @@
-(function() {
+(function () {
 
-    "use strict";
+  'use strict';
 
-    var gulp = require('gulp'),
-        sass = require('gulp-sass'),
-        compass = require('gulp-compass'),
-        minifyCss = require('gulp-minify-css'),
-        browserSync = require('browser-sync'),
-        uglify = require('gulp-uglify'),
-        swig = require('gulp-swig'),
-        concat = require('gulp-concat'),
-        glob = require('glob'),
-        changed = require('gulp-changed'),
-        del = require('del'),
-        plumber = require('gulp-plumber'),
-        autoprefixer = require('gulp-autoprefixer');
+  const gulp = require('gulp');
+  const sass = require('gulp-sass');
+  const minifyCss = require('gulp-clean-css');
+  const browserSync = require('browser-sync');
+  const uglify = require('gulp-uglify');
+  const swig = require('gulp-swig');
+  const concat = require('gulp-concat');
+  const changed = require('gulp-changed');
+  const del = require('del');
+  const plumber = require('gulp-plumber');
+  const autoprefixer = require('gulp-autoprefixer');
 
+  // Convierto SCSS a CSS, minimizo, concateno y copio el archivo style.css a dist
+  gulp.task('scss', function () {
+    gulp.src('scss/style.scss')
+      .pipe(plumber())
+      .pipe(sass({
+        includePaths: [
+          'scss',
+          'bower_components/bootstrap-sass-official/assets/stylesheets',
+          'bower_components/fontawesome/scss',
+        ],
+      }))
+      .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+      .pipe(minifyCss({
+        keepSpecialComments: 0,
+      }))
+      .pipe(concat('style.css'))
+      .pipe(gulp.dest('dist/css'));
+  });
 
-    // Convierto SCSS a CSS, minimizo, concateno y copio el archivo style.css a build
-    gulp.task('scss', function () {
-        gulp.src('scss/style.scss')
-            .pipe(plumber())
-            .pipe(sass({
-                includePaths: [
-                    'scss',
-                    'bower_components/bootstrap-sass-official/assets/stylesheets',
-                    'bower_components/fontawesome/scss'
-                ]
-            }))
-            .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-            .pipe(minifyCss({
-                keepSpecialComments: 0
-            }))
-            .pipe(concat('style.css'))
-            .pipe(gulp.dest('build/css'));
+  // Minimizo el javascript, lo concateno en app.js y lo copio a dist
+  gulp.task('javascript', function () {
+    gulp.src([
+        'bower_components/jquery/dist/jquery.js',
+        'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
+        'js/*.js',
+      ])
+      //.pipe(uglify())
+      .pipe(concat('app.js'))
+      .pipe(gulp.dest('dist/js'));
+  });
+
+  // Construyo los HTML a partir de las plantillas Swig y las copio a dist
+  gulp.task('templates', function () {
+    gulp.src('templates/*.html')
+      .pipe(swig({
+        load_json: true,
+        json_path: 'templates',
+        defaults: {
+          cache: false,
+        },
+      }))
+      .pipe(gulp.dest('dist'));
+  });
+
+  // Copio las imágenes a dist
+  gulp.task('images', function () {
+    gulp.src('img/**')
+      .pipe(changed('dist/img/**'))
+      .pipe(gulp.dest('dist/img'));
+  });
+
+  // Copio las fuentes a dist
+  gulp.task('fonts', function () {
+    gulp.src('bower_components/fontawesome/fonts/**')
+      .pipe(changed('dist/fonts/**'))
+      .pipe(gulp.dest('dist/fonts'));
+  });
+
+  // Mantengo un servidor httpd que hace que el navegador se recargue cuando detecta cambios
+  gulp.task('browser-sync', function () {
+    browserSync.init(['dist/css/*.css', 'dist/js/*.js', 'dist/*.html', 'dist/img/**'], {
+      server: {
+        baseDir: 'dist',
+      },
     });
+  });
 
-    // Minimizo el javascript, lo concateno en app.js y lo copio a build
-    gulp.task('javascript', function () {
-        gulp.src([
-            "bower_components/jquery/dist/jquery.js",
-            //"bower_components/jqueryui/jquery-ui.js",
-            "bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js",
-            "js/*.js"
-        ])
-            .pipe(uglify())
-            .pipe(concat('app.js'))
-            .pipe(gulp.dest('build/js'));
-    });
+  // Elimino el contenido de la carpeta dist
+  gulp.task('clean', function () {
+    del(['dist/**']);
+  });
 
-    // Construyo los HTML a partir de las plantillas Swig y las copio a build
-    gulp.task('templates', function () {
-        gulp.src('templates/*.html')
-            .pipe(swig({
-                load_json: true,
-                json_path: 'templates',
-                defaults: {
-                    cache: false
-                }
-            }))
-            .pipe(gulp.dest('build'));
-    });
-
-    // Copio las imágenes a build
-    gulp.task('images', function () {
-        gulp.src('img/**')
-            .pipe(changed('build/img/**'))
-            .pipe(gulp.dest('build/img'));
-    });
-
-    // Copio las fuentes a build
-    gulp.task('fonts', function () {
-      gulp.src('bower_components/fontawesome/fonts/**')
-      .pipe(changed('build/fonts/**'))
-      .pipe(gulp.dest('build/fonts'));
-    });
-
-    // Mantengo un servidor httpd que hace que el navegador se recargue cuando detecta cambios
-    gulp.task('browser-sync', function () {
-        browserSync.init(["build/css/*.css", "build/js/*.js", "build/*.html", "build/img/**"], {
-            server: {
-                baseDir: "build"
-            }
-        });
-    });
-
-    // Elimino el contenido de la carpeta build
-    gulp.task('clean', function () {
-        del(['build/**']);
-    });
-
-    // Tarea por defecto para desarrollo
-    gulp.task('default', ['scss', 'javascript', 'templates', 'images', 'browser-sync'], function () {
-        gulp.watch("scss/*.scss", ['scss']);
-        gulp.watch("js/*.js", ['javascript']);
-        gulp.watch("templates/**", ['templates']);
-    });
-
+  // Tarea por defecto para desarrollo
+  gulp.task('default', ['scss', 'javascript', 'templates', 'images', 'browser-sync'], function () {
+    gulp.watch('scss/*.scss', ['scss']);
+    gulp.watch('js/*.js', ['javascript']);
+    gulp.watch('templates/**', ['templates']);
+  });
 
 })();
--- js/script.js
+++ js/script.js
@@ -1,27 +1,29 @@
 /*jshint browser: true */
 /*global $*/
 
+(function () {
+
   'use strict';
 
-  $(document).ready(function(){
+  $(document).ready(function () {
 
-    var cellStyle = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight"];
+    var cellStyle = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'];
 
-    var nMines = 10,
-        boardSize = 8,
-        timer;
+    var nMines = 10;
+    var boardSize = 8;
+    let timer;
 
-    var $board = $(".board"),
-        $time = $(".time"),
-        $message = $(".message");
+    var $board = $('.board');
+    var $time = $('.time');
+    var $message = $('.message');
 
     init();
 
-    $(".again").on("click", restart);
+    $('.again').on('click', restart);
 
     function restart() {
-      $board.find("td").removeData("mines").removeData("revealed").text("").removeClass().removeAttr("style");
-      $message.removeClass("visible");
+      $board.find('td').removeData('mines').removeData('revealed').text('').removeClass().removeAttr('style');
+      $message.removeClass('visible');
       init();
     }
 
@@ -30,59 +32,68 @@
       var n = 0;
 
       // Start a counter in seconds
-      $time.text("0");
-      timer = setInterval(function() {
+      $time.text('0');
+      timer = setInterval(function () {
         $time.text(+$time.text() + 1);
       }, 1000);
 
       // Populate the board with mines in random positions
-      while(n < nMines) {
+      while (n < nMines) {
         var x = randomNumber(boardSize);
         var y = randomNumber(boardSize);
 
-        var $cell = $board.find("tr").eq(y).find("td").eq(x);
+        var $cell = $board.find('tr').eq(y).find('td').eq(x);
 
         // Place a mine in the generated position only if there is no mine previously placed
-        if($cell.data("mines") !== 0) {
-          $cell.data("mines", 0);
+        if ($cell.data('mines') !== 0) {
+          $cell.data('mines', 0);
           n++;
         }
       }
 
       // For each field look all around searching for how many mines are, and set this
       // number as a data value
-      for(var i = 0; i < boardSize; i++) {
-        for(var j = 0; j < boardSize; j++) {
+      for (var i = 0; i < boardSize; i++) {
+        for (var j = 0; j < boardSize; j++) {
           var total = 0;
-          var $field = $board.find("tr").eq(i).find("td").eq(j);
-          $field.data("revealed", false);
-          if($field.data("mines") !== 0) {
-            if(i-1 >= 0 && j-1 >= 0) {
-              if($board.find("tr").eq(i-1).find("td").eq(j-1).data("mines") === 0) total++;
+          var $field = $board.find('tr').eq(i).find('td').eq(j);
+          $field.data('revealed', false);
+          if ($field.data('mines') !== 0) {
+
+            if (i - 1 >= 0 && j - 1 >= 0) {
+              if ($board.find('tr').eq(i - 1).find('td').eq(j - 1).data('mines') === 0) total++;
             }
-            if(i-1 >= 0) {
-              if($board.find("tr").eq(i-1).find("td").eq(j).data("mines") === 0) total++;
+
+            if (i - 1 >= 0) {
+              if ($board.find('tr').eq(i - 1).find('td').eq(j).data('mines') === 0) total++;
             }
-            if(i-1 >= 0 && j+1 <= boardSize) {
-              if($board.find("tr").eq(i-1).find("td").eq(j+1).data("mines") === 0) total++;
+
+            if (i - 1 >= 0 && j + 1 <= boardSize) {
+              if ($board.find('tr').eq(i - 1).find('td').eq(j + 1).data('mines') === 0) total++;
             }
-            if(j-1 >= 0) {
-              if($board.find("tr").eq(i).find("td").eq(j-1).data("mines") === 0) total++;
+
+            if (j - 1 >= 0) {
+              if ($board.find('tr').eq(i).find('td').eq(j - 1).data('mines') === 0) total++;
             }
-            if(j+1 <= boardSize) {
-              if($board.find("tr").eq(i).find("td").eq(j+1).data("mines") === 0) total++;
+
+            if (j + 1 <= boardSize) {
+              if ($board.find('tr').eq(i).find('td').eq(j + 1).data('mines') === 0) total++;
             }
-            if(i+1 <= boardSize && j-1 >= 0) {
-              if($board.find("tr").eq(i+1).find("td").eq(j-1).data("mines") === 0) total++;
+
+            if (i + 1 <= boardSize && j - 1 >= 0) {
+              if ($board.find('tr').eq(i + 1).find('td').eq(j - 1).data('mines') === 0) total++;
             }
-            if(i+1 <= boardSize) {
-              if($board.find("tr").eq(i+1).find("td").eq(j).data("mines") === 0) total++;
+
+            if (i + 1 <= boardSize) {
+              if ($board.find('tr').eq(i + 1).find('td').eq(j).data('mines') === 0) total++;
             }
-            if(i+1 <= boardSize && j+1 <= boardSize) {
-              if($board.find("tr").eq(i+1).find("td").eq(j+1).data("mines") === 0) total++;
+
+            if (i + 1 <= boardSize && j + 1 <= boardSize) {
+              if ($board.find('tr').eq(i + 1).find('td').eq(j + 1).data('mines') === 0) total++;
             }
-            if(total > 0){
-              $field.data("mines", total);
+
+            if (total > 0) {
+              $field.data('mines', total);
             }
           }
         }
@@ -90,68 +101,74 @@
     }
 
     // Reveal all the board (PROVISIONAL, only for debug purposes)
-    $("h1").on("click", function() {
-      $board.find("td").each(function(index) {
+    $('h1').on('click', function () {
+      $board.find('td').each(function () {
         revealField($(this));
       });
     });
 
     /*
-    * Reveal all the fields containing a mine
-    */
+     * Reveal all the fields containing a mine
+     */
     function revealAllMines() {
-      for(var i = 0; i < boardSize; i++) {
-        for(var j = 0; j < boardSize; j++) {
-          var $field = $board.find("tr").eq(i).find("td").eq(j);
-          if($field.data("mines") === 0) {
+      for (var i = 0; i < boardSize; i++) {
+        for (var j = 0; j < boardSize; j++) {
+          var $field = $board.find('tr').eq(i).find('td').eq(j);
+          if ($field.data('mines') === 0) {
             revealField($field);
           }
         }
       }
-      //$board.find("td").unbind("click");
+      //$board.find('td').unbind('click');
       clearInterval(timer);
-      $message.addClass("visible");
+      $message.addClass('visible');
     }
 
     /*
-    * Show what's hidden behind a field (a mine, a number or an empty field)
-    */
+     * Show what's hidden behind a field (a mine, a number or an empty field)
+     */
     function revealField($field) {
-      $field.text($field.data("mines")).data("revealed", true);
-      if(typeof $field.data("mines") != "undefined") {
-        $field.addClass(cellStyle[$field.data("mines")]);
+      $field.text($field.data('mines')).data('revealed', true);
+      if (typeof $field.data('mines') !== 'undefined') {
+        $field.addClass(cellStyle[$field.data('mines')]);
       } else {
         $field.css({
-          "background-color": "#eee"
+          'background-color': '#eee',
         });
       }
     }
 
     // Click event on any field. It has to reveal a number, a mine, or all the empty
     // fields around if it is an empty one itself
-    $board.find("td").on("click", clickCell);
+    $board.find('td').on('click', clickCell);
 
     function clickCell(event) {
       var $field = $(event.target);
-      if($field.data("mines") === 0) {
+      if ($field.data('mines') === 0) {
         revealAllMines();
-      } else if(typeof $field.data("mines") !== "undefined") {
+      } else if (typeof $field.data('mines') !== 'undefined') {
         revealField($field);
       } else {
         revealField($field);
 
-        var f=[], g, k, l, n=0;
+        var f = [];
+        var g = 0;
+        var k = 0;
+        var l = 0;
+        var n = 0;
+
         f[n] = getVoidsAround($field);
 
         do {
-          f[n+1] = [];
-          for(k=0; k<f[n].length; k++) {
-            g = getVoidsAround($board.find("tr").eq(f[n][k].x).find("td").eq(f[n][k].y));
-            for(l=0; l<g.length; l++) {
-              f[n+1].push(g[l]);
+          f[n + 1] = [];
+          for (k = 0; k < f[n].length; k++) {
+            g = getVoidsAround($board.find('tr').eq(f[n][k].x).find('td').eq(f[n][k].y));
+            for (l = 0; l < g.length; l++) {
+              f[n + 1].push(g[l]);
             }
           }
-          window.console.log(JSON.stringify(f[n+1])); // debug
+
+          window.console.log(JSON.stringify(f[n + 1])); // debug
           n++;
         } while (f[n].length > 0);
 
@@ -172,62 +189,102 @@
       var $voids = [];
 
       // Reveal all the adjacent fields (when exist) and search for voids
-      if(i-1 >= 0 && j-1 >= 0) {
-        var $field1 = $board.find("tr").eq(i-1).find("td").eq(j-1);
-        if(typeof $field1.data("mines") == "undefined" && $field1.data("revealed") === false) {
-          $voids.push({"x":i-1, "y":j-1});
+      if (i - 1 >= 0 && j - 1 >= 0) {
+        var $field1 = $board.find('tr').eq(i - 1).find('td').eq(j - 1);
+        if (typeof $field1.data('mines') === 'undefined' && !$field1.data('revealed')) {
+          $voids.push({
+            x: i - 1,
+            y: j - 1,
+          });
         }
+
         revealField($field1);
       }
-      if(i-1 >= 0) {
-        var $field2 = $board.find("tr").eq(i-1).find("td").eq(j);
-        if(typeof $field2.data("mines") == "undefined" && $field2.data("revealed") === false) {
-          $voids.push({"x":i-1, "y":j});
+
+      if (i - 1 >= 0) {
+        var $field2 = $board.find('tr').eq(i - 1).find('td').eq(j);
+        if (typeof $field2.data('mines') === 'undefined' && !$field2.data('revealed')) {
+          $voids.push({
+            x: i - 1,
+            y: j,
+          });
         }
+
         revealField($field2);
       }
-      if(i-1 >= 0 && j+1 <= boardSize) {
-        var $field3 = $board.find("tr").eq(i-1).find("td").eq(j+1);
-        if(typeof $field3.data("mines") == "undefined" && $field3.data("revealed") === false) {
-          $voids.push({"x":i-1, "y":j+1});
+
+      if (i - 1 >= 0 && j + 1 <= boardSize) {
+        var $field3 = $board.find('tr').eq(i - 1).find('td').eq(j + 1);
+        if (typeof $field3.data('mines') === 'undefined' && !$field3.data('revealed')) {
+          $voids.push({
+            x: i - 1,
+            y: j + 1,
+          });
         }
+
         revealField($field3);
       }
-      if(j-1 >= 0) {
-        var $field4 = $board.find("tr").eq(i).find("td").eq(j-1);
-        if(typeof $field4.data("mines") == "undefined" && $field4.data("revealed") === false) {
-          $voids.push({"x":i, "y":j-1});
+
+      if (j - 1 >= 0) {
+        var $field4 = $board.find('tr').eq(i).find('td').eq(j - 1);
+        if (typeof $field4.data('mines') === 'undefined' && !$field4.data('revealed')) {
+          $voids.push({
+            x: i,
+            y: j - 1,
+          });
         }
+
         revealField($field4);
       }
-      if(j+1 <= boardSize) {
-        var $field5 = $board.find("tr").eq(i).find("td").eq(j+1);
-        if(typeof $field5.data("mines") == "undefined" && $field5.data("revealed") === false) {
-          $voids.push({"x":i, "y":j+1});
+
+      if (j + 1 <= boardSize) {
+        var $field5 = $board.find('tr').eq(i).find('td').eq(j + 1);
+        if (typeof $field5.data('mines') === 'undefined' && !$field5.data('revealed')) {
+          $voids.push({
+            x: i,
+            y: j + 1,
+          });
         }
+
         revealField($field5);
       }
-      if(i+1 <= boardSize && j-1 >= 0) {
-        var $field6 = $board.find("tr").eq(i+1).find("td").eq(j-1);
-        if(typeof $field6.data("mines") == "undefined" && $field6.data("revealed") === false) {
-          $voids.push({"x":i+1, "y":j-1});
+
+      if (i + 1 <= boardSize && j - 1 >= 0) {
+        var $field6 = $board.find('tr').eq(i + 1).find('td').eq(j - 1);
+        if (typeof $field6.data('mines') === 'undefined' && !$field6.data('revealed')) {
+          $voids.push({
+            x: i + 1,
+            y: j - 1,
+          });
         }
+
         revealField($field6);
       }
-      if(i+1 <= boardSize) {
-        var $field7 = $board.find("tr").eq(i+1).find("td").eq(j);
-        if(typeof $field7.data("mines") == "undefined" && $field7.data("revealed") === false) {
-          $voids.push({"x":i+1, "y":j});
+
+      if (i + 1 <= boardSize) {
+        var $field7 = $board.find('tr').eq(i + 1).find('td').eq(j);
+        if (typeof $field7.data('mines') === 'undefined' && !$field7.data('revealed')) {
+          $voids.push({
+            x: i + 1,
+            y: j,
+          });
         }
+
         revealField($field7);
       }
-      if(i+1 <= boardSize && j+1 <= boardSize) {
-        var $field8 = $board.find("tr").eq(i+1).find("td").eq(j+1);
-        if(typeof $field8.data("mines") == "undefined" && $field8.data("revealed") === false) {
-          $voids.push({"x":i+1, "y":j+1});
+
+      if (i + 1 <= boardSize && j + 1 <= boardSize) {
+        var $field8 = $board.find('tr').eq(i + 1).find('td').eq(j + 1);
+        if (typeof $field8.data('mines') === 'undefined' && !$field8.data('revealed')) {
+          $voids.push({
+            x: i + 1,
+            y: j + 1,
+          });
         }
+
         revealField($field8);
       }
+
       return $voids;
     }
 
@@ -237,3 +294,5 @@
   function randomNumber(boardSize) {
     return Math.floor((Math.random() * boardSize));
   }
+
+})();
